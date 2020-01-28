c8ee9eadb6bdb66885331d12a47603e5b81ef82b
itsmeJustinC <36489899+itsmeJustinC@users.noreply.github.com>
2020-01-28 18:21:54 -0500

    Add comments

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -2,13 +2,18 @@ package frc.robot;
 
 
 public class RobotMap {
+	// Victor motors
 	public static final int masterMotorLeftId = 0;
 	public static final int slaveMotorLeftId = 1;
 	public static final int masterMotorRightId = 2;
 	public static final int slaveMotorRightId = 3;
+	
+	// Joystick
 	public static final int joystickId = 0;
 	public static final int leftStickYAxisId = 1;
 	public static final int rightStickXAxisId = 4;
+	
+	// Controller triggers and buttons
 	public static final int leftTriggerId = 2;
 	public static final int rightTriggerId = 3;
 }
